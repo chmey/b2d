2591b5cfb07fa69d1b8d4bc64e2cc585a96d114f
Johannes Willbold <johannes.willbold@rub.de>
2020-02-18 14:10:28 +0100

    Init

---
+++ backend/index.js
@@ -0,0 +1,83 @@
+const WebSocket = require('ws');
+const Influx = require('influx');
+const os = require('os');
+
+
+const influx = new Influx.InfluxDB({
+  host: 'localhost',
+  database: 'b2dth_db',
+  port: 8086,
+  username: 'connor',
+  password: 'pa$$w0rd',
+  schema: [
+   {
+     measurement: 'b2dping',
+     fields: {
+       tool_bd_addr: Influx.FieldType.STRING,
+       receiver_bd_addr: Influx.FieldType.STRING,
+       signal_strength: Influx.FieldType.INTEGER
+     },
+     tags: [
+       'host'
+     ]
+   }
+  ]
+});
+
+influx.createDatabase('b2dth_db');
+
+// SELECT signal_strength FROM b2dping WHERE receiver_bd_addr='XXX' AND time >= 'XXX' AND time <= 'XXX'
+
+influx.writePoints([
+  {
+    measurement: 'b2dping',
+    tags: { host: os.hostname() },
+    // fields: { duration, path: req.path },
+    fields: { tool_bd_addr: "some:tool", receiver_bd_addr: "some:path", signal_strength: 100 },
+  }
+]).then(() => {
+  return influx.query(`
+    select * from b2dping
+    where host = ${Influx.escape.stringLit(os.hostname())}
+    order by time desc
+    limit 10
+  `)
+}).then(rows => {
+  rows.forEach(row => console.log(`A request to ${row.tool_bd_addr} has a signal strength of ${row.signal_strength}`))
+});
+
+
+
+const wss = new WebSocket.Server({ port: 8081 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
