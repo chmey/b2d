d49ddd761d13048eca3cce995661d90ef84171d0
Christoph Meyer <christoph.meyerpr@gmail.com>
2020-02-18 19:51:02 +0100

    Removed obsolete comments

--- backend/index.js
+++ backend/index.js
@@ -42,21 +42,13 @@ wss.on('connection', function connection(ws) {
         tags: { tool_id: data.tool_id, recv_bd_addr: data.recv_bd_addr },
         fields: {rssi: data.rssi },
       }
-    ]).then(_ => {
-      influx.query(`SELECT mean("signal_strength") FROM "b2dping" WHERE ("host" = 'L480') GROUP BY "major", "minor"`).then(rows => {
-      //     // rows are unique tool_bd_addr
-      rows.forEach(row => console.log(row))
-          // rows.forEach(row => console.log(`TOOL: ${row.tool_bd_addr} ${row.mean} avg`))
-        // rows.forEach(row => console.log(`${row.uuid} ${row.major}:${row.minor} - tool ${row.tool_bd_addr} to receiver ${row.receiver_bd_addr} has a signal strength of ${row.signal_strength}`))
-      });
-    });
+    ])
   });
 
   ws.on("close", function() {
    console.log("Closing...");
  });
 
-  // ws.send('something');
 });
 
 server.listen(8081);
