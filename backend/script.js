91efaf2064d0834207536a9637d678d36629d486
Christoph Meyer <christoph.meyerpr@gmail.com>
2020-02-19 10:13:35 +0100

    Demo function

--- backend/script.js
+++ backend/script.js
@@ -10,8 +10,43 @@ const recvColor = "blue";
 
 const toolRadius = 15;
 const toolColor = 'orange';
+const toolTrack = [
+                    // idle in middle
+                    {x: 400, y: 300}, {x:410, y:290}, {x: 390, y: 310},
+                    {x: 380, y: 300}, {x:390, y:280}, {x: 375, y: 290},
+                    {x: 385, y: 290}, {x:390, y:280}, {x: 400, y: 310},
+                    {x: 405, y: 315}, {x:400, y:310}, {x: 410, y: 310},
+                    {x: 415, y: 305}, {x:420, y:300}, {x: 410, y: 310},
+                    // walk with me
+                    {x: 415, y: 300}, {x:425, y:305}, {x: 435, y: 305},
+                    {x: 445, y: 300}, {x:455, y:305}, {x: 465, y: 310},
+                    {x: 475, y: 310}, {x:480, y:315}, {x: 485, y: 310},
+                    {x: 495, y: 305}, {x:505, y:295}, {x: 515, y: 300},
+                    {x: 525, y: 300}, {x:555, y:305}, {x: 575, y: 300},
 
-const simulate = false;
+                    {x: 565, y: 310}, {x:585, y:320}, {x: 595, y: 330},
+                    {x: 595, y: 350}, {x:600, y:325}, {x: 585, y: 300},
+                    {x: 600, y: 300}, {x:600, y:305}, {x: 620, y: 300},
+
+                    {x: 630, y: 295}, {x:635, y:280}, {x: 655, y: 300},
+                    {x: 680, y: 310}, {x:670, y:305}, {x: 650, y: 300},
+                    {x: 640, y: 290}, {x:650, y:285}, {x: 655, y: 275},
+                    {x: 635, y: 260}, {x:630, y:275}, {x: 640, y: 280},
+                    {x: 645, y: 270}, {x:620, y:250}, {x: 630, y: 240},
+                    {x: 645, y: 250}, {x:625, y:245}, {x: 640, y: 240},
+                    {x: 650, y: 235}, {x:645, y:225}, {x: 640, y: 230},
+                    {x: 645, y: 270}, {x:620, y:250}, {x: 630, y: 240},
+                    {x: 645, y: 250}, {x:625, y:245}, {x: 640, y: 240},
+                    {x: 650, y: 235}, {x:645, y:225}, {x: 640, y: 230},
+
+
+
+
+
+
+                  ]
+var seriesIndex = 0;
+const simulate = true;
 
 function drawDot(x, y, r, color) {
   ctx.beginPath();
@@ -31,14 +66,21 @@ function update() {
 
 
   if(simulate) {
-    drawDot(Math.random()*(bw-recvMargin)+recvMargin, Math.random()*(bh-recvMargin)+recvMargin, toolRadius, toolColor);
+    drawDot(
+      toolTrack[seriesIndex % toolTrack.length].x,
+      toolTrack[seriesIndex++ % toolTrack.length].y,
+      toolRadius,
+      toolColor
+    );
+    // drawDot(
+      // Math.random() * (bw-recvMargin)+recvMargin,
+      // Math.random() * (bh-recvMargin)+recvMargin, toolRadius, toolColor
+    // );
   } else {
     drawDot(400, 300, toolRadius, toolColor);
   }
 }
 
 
-// setInterval(update, 200);
-update();
-
-
+setInterval(update, 350);
+// update();
